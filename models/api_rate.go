b86b182bcee18ea8e8d4836da5440813fc9e4426
Lucas Lacerda <146731273+llacerdapaypal@users.noreply.github.com>
2023-12-05 16:27:11 -0300

    fix: weight fail safe replacing commodities weight (#41)

--- models/api_rate.go
+++ models/api_rate.go
@@ -70,7 +70,6 @@ func (r *Rate) Weight() Weight {
 		// heavy enough that the destination will matter when choosing between two
 		// fedex ground rates
 		commoditiesWeight.Value = math.Min(commoditiesWeight.Value, 150.0)
-		commoditiesWeight.Value = math.Max(commoditiesWeight.Value, 13.0)
 		return commoditiesWeight
 	}
 
